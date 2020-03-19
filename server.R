8e03a7d91bfc5c15119938c2752967e708a0501a
papiyachatterjee <papiya.chatterjee@gmail.com>
2020-03-19 17:19:26 +0530

    Update server.R

--- server.R
+++ server.R
@@ -93,17 +93,12 @@ mycorpus=tm_map(mycorpus,function(x) removeWords(x,"x"))
 #make a document term matrix now
 dtm=as.matrix(DocumentTermMatrix(mycorpus))
                 
-           article_words = article_sentences() %>%
-            unnest_tokens(word, sentence) %>%
+article_words = article_sentences() %>%
+ unnest_tokens(word, sentence) %>%
             # drop stopwords
             anti_join(stop_words, by = "word")             
-          article_summary <- textrank_sentences(data = article_sentences(), 
-                                              terminology = article_words)
-                
-            summ_sents = article_summary %>%
-            arrange(desc(textrank)) %>% 
-            slice(1:input$num) %>%  # dplyr::slice() chooses rows by their ordinal position in the tbl
-            pull(sentence) %>% tibble()
+article_summary <- textrank_sentences(data = article_sentences(), 
+                                              terminology = article_words)         
 
 myvector=c(summ_sents)
 #making corpus of two documents
